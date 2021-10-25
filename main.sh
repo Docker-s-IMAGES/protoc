55805f26855b35cfa06b3a200aa814175c6484a2
Aber <me@abersheeran.com>
2021-10-25 17:24:12 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 protoc -I /src --python_betterproto_out=lib $*
 # Merge python file to module that has the same name
-/usr/bin/python3 /usr/bin/merge-python.py
+/usr/bin/python3 /usr/bin/merge-python.py lib
